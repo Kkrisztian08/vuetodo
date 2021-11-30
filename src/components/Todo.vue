40289266e166223ab6aabcfddb9aa4aaf1d924ca
Kkrisztian08 <k.krisztian0830@gmail.com>
2021-11-30 14:15:33 +0100

    Todod lista mentése

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-      Todo
+      <ul>
+          <li v-for="todo in todos" v-bind:key="todo.title">
+              {{ todo.title }}
+          </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+    props:['todos']
 }
 </script>
 
